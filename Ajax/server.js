9b91a6d9447ebf0dccaefc883f9f50bacb4a0094
Antônio Augusto Zamai Amaral <51514805+agamaral@users.noreply.github.com>
2020-01-24 14:50:35 -0300

    parte 2 http request xmp

--- Ajax/server.js
+++ Ajax/server.js
@@ -6,6 +6,25 @@ app.use(express.static('.')) //sirva os arquivos estáticos!
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-//app.get('/teste', (req,res)=> res.send('Ok.')) //quando o /test for invocado atraves d get
+let multer = require('multer');
+
+let storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null,'./upload')
+    },
+    filename: function(req,file,callback) {
+        callback(null, `${Date.now()}_${file.originalName}`)
+    }
+})
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload',(req,res)=> {
+    upload(req,res,err => {
+        if (err) {
+            return res.end('erro')
+        }
+        res.end('concluido.')
+    })
+})
 
 app.listen(8081, ()=> console.log('executando server...'))
