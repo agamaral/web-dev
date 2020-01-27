00908913959ca59e7630684197070227cf8e8506
Antônio Augusto Zamai Amaral <51514805+agamaral@users.noreply.github.com>
2020-01-27 10:58:21 -0300

    axios 2

--- Ajax/server.js
+++ Ajax/server.js
@@ -3,23 +3,23 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('.')) //sirva os arquivos estáticos!
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 let multer = require('multer');
 
 let storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null,'./upload')
+        callback(null, './upload')
     },
-    filename: function(req,file,callback) {
+    filename: function (req, file, callback) {
         callback(null, `${Date.now()}_${file.originalName}`)
     }
 })
-const upload = multer({storage}).single('arquivo')
+const upload = multer({ storage }).single('arquivo')
 
-app.post('/upload',(req,res)=> {
-    upload(req,res,err => {
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
         if (err) {
             return res.end('erro')
         }
@@ -27,11 +27,18 @@ app.post('/upload',(req,res)=> {
     })
 })
 
-app.post('/formulario', (req, res)=> {
+app.post('/formulario', (req, res) => {
     res.send({
         ...req.body,
         id: 7
     })
 })
 
-app.listen(8081, ()=> console.log('executando server...'))
+app.get('/parOuImpar', (req, res) => {
+    const par = parseInt(req.query.numero) % 2 ===0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
+app.listen(8081, () => console.log('executando server...'))
