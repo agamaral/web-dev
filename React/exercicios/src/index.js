943c8be1c5915ce86b700dbf5b08fd229a2204a6
Antônio Augusto Zamai Amaral <51514805+agamaral@users.noreply.github.com>
2020-01-30 15:56:46 -0300

    Primeiro componente

--- React/exercicios/src/index.js
+++ React/exercicios/src/index.js
@@ -1,4 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import _primeiro from './componentes/primeiro'
 
-ReactDOM.render(<h1>React 2</h1>, document.getElementById('root'))
+// o CORNO do leo nao disse, mas tem que chamar com esse '_' antes, pra funfar :)
+
+ReactDOM.render(<_primeiro/>, document.getElementById('root'))
