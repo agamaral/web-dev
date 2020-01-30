a95a059c9d3a5032e9859f2823d88ccbf161accb
Antônio Augusto Zamai Amaral <51514805+agamaral@users.noreply.github.com>
2020-01-30 16:11:53 -0300

    Componente com parâmetro

--- React/exercicios/src/index.js
+++ React/exercicios/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import _primeiro from './componentes/primeiro'
+
+import _BomDia from './componentes/BomDia'
 
 // o CORNO do leo nao disse, mas tem que chamar com esse '_' antes, pra funfar :)
 
-ReactDOM.render(<_primeiro/>, document.getElementById('root'))
+ReactDOM.render(<_BomDia nome="zezé"/>, document.getElementById('root'))
