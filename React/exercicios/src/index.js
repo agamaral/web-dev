6c9feeb4cf6e96e7b84fccaa9bfa24c01d83d44b
Antônio Augusto Zamai Amaral <51514805+agamaral@users.noreply.github.com>
2020-01-31 11:49:53 -0300

    Final do capítulo de react

--- React/exercicios/src/index.js
+++ React/exercicios/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Multi from './componentes/Multiplos'
+import _Pai from './componentes/Pai'
+import _Filho from './componentes/Filho'
 
 ReactDOM.render (
     <div>
-        <Multi.BoaTarde nome="Núbia Helena"/>
-        <Multi.BoaTarde nome="Antônio Augusto"/>
+        <_Pai nome ="Jander" sobrenome="Pereira">
+            <_Filho nome="Renan"/>
+            <_Filho nome="Nubia"/>
+        </_Pai>
     </div>
-, document.getElementById('root'))
+,document.getElementById('root'))
